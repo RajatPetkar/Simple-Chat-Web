5497cfb6729f236b7dea8260061994aae2567181
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2023-07-22 12:23:39 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000')
+const socket = io('https://simple-chat-or1n.onrender.com/')
 const messageContainer = document.getElementById('message-container')
 const roomContainer = document.getElementById('room-container')
 const messageForm = document.getElementById('send-container')
