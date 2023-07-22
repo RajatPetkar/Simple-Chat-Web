9a8fff218b6c24025fb34034ddabab2bf007e580
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2023-07-22 12:18:43 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,47 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const roomContainer = document.getElementById('room-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+if (messageForm != null) {
+  const name = prompt('What is your name?')
+  appendMessage('You joined')
+  socket.emit('new-user', roomName, name)
+
+  messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', roomName, message)
+    messageInput.value = ''
+  })
+}
+
+socket.on('room-created', room => {
+  const roomElement = document.createElement('div')
+  roomElement.innerText = room
+  const roomLink = document.createElement('a')
+  roomLink.href = `/${room}`
+  roomLink.innerText = 'join'
+  roomContainer.append(roomElement)
+  roomContainer.append(roomLink)
+})
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+  appendMessage(`${name} joined the room`)
+})
+
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} left the room`)
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
+}
